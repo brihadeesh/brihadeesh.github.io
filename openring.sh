069b3010656cd273aeaa2a8e8a76377f34699661
Brihadeesh S <brihadeesh@protonmail.com>
2025-02-12 03:05:27 +0530

    i think i've fixed openring

---
+++ openring.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+FEEDLIST=$1
+INPUT_TEMPLATE=$2
+
+readarray -t FEEDS < $FEEDLIST
+
+# populated below
+OPENRING_ARGS=""
+
+for FEED in "${FEEDS[@]}"
+do
+    OPENRING_ARGS="$OPENRING_ARGS -s $FEED"
+done
+
+openring $OPENRING_ARGS < $INPUT_TEMPLATE
